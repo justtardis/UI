79d6f4b9b687c7cff681b6c82a6b249365e6a9b2
Just <justtardis@gmail.com>
2020-05-28 01:43:38 +1000

    Animation UI

---
+++ Assets/scripts/DiagramFiller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiagramFiller : MonoBehaviour
+{
+    [SerializeField] private Image _diagramFill;
+
+    [Range(0f, 1f)] [SerializeField] private float _currentCount;
+    [SerializeField] private UIChanger _uIChanger;
+
+    private float _previousCount;
+
+    private void Start()
+    {
+        _previousCount = _currentCount;
+        Filler();
+    }
+
+    private void Filler()
+    {
+        _diagramFill.fillAmount = _currentCount;
+        _uIChanger.TextPercentFiller(_currentCount);
+    }
+   
+    private void CheckChangedValue()
+    {
+        if (_currentCount != _previousCount)
+        {
+            Filler();
+            _previousCount = _currentCount;
+        }
+    }
+
+    private void Update()
+    {
+        CheckChangedValue();
+    }
+}
---
+++ Assets/scripts/UIChanger.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIChanger : MonoBehaviour
+{
+    [SerializeField] private Text _percentTextFiller;
+
+    public void ClickPlay(Animator g_animator)
+    {
+       g_animator.SetTrigger("isCompressed");
+    }
+
+    public void ClickSettings(Animator g_animator)
+    {
+        g_animator.SetTrigger("isColored");
+    }
+
+    public void ClickCredits(Animator g_animator)
+    {
+        g_animator.SetTrigger("isWiggle");
+    }
+
+    public void ClickSteam(Animator g_animator)
+    {
+        g_animator.SetTrigger("isRotateIcon");
+    }
+
+    public void ClickShare(Animator g_animator)
+    {
+        g_animator.SetTrigger("isJump");
+    }
+
+    public void ClickRefresh(Animator g_animator)
+    {
+        g_animator.SetTrigger("isRefresh");
+    }
+
+    public void ClickExit(Animator g_animator)
+    {
+        g_animator.SetTrigger("isExit");
+    }
+
+    public void TextPercentFiller(float value)
+    {
+        _percentTextFiller.text = $"<size=110>{(int)(value*100)}</size><size=60>%</size>";
+    }
+}
